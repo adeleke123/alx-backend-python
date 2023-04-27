8586b2a8c8009022d45373c23deb2fe7b1aca3c8
Bakare Muideen Adeleke <51156057+adeleke123@users.noreply.github.com>
2023-04-27 10:42:56 +0100

    python variable annotations

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Contains a function that sums a list of floats"""
+"""Contains a function that sums a list of floats."""
 from typing import List
 
 
@@ -8,7 +8,7 @@ def sum_list(input_list: List[float]) -> float:
     Args:
         input_list (list): A list of floats
     Returns:
-        float: The sum of the floats in the list
+        float: The sum of the floats in the list.
     """
     if input_list is None:
         return 0
