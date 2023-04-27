832cf414b69fd818be97b2c6eb3b2d33d254eb21
Bakare Muideen Adeleke <51156057+adeleke123@users.noreply.github.com>
2023-04-27 10:06:09 +0100

    python variable annotations

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-"""Contains a single function that concatenates two strings."""
+"""Contains a single function that concatenates two strings"""
 
 
 def concat(str1: str, str2: str) -> str:
-    """Concatenates two strings."""
+    """Concatenates two strings"""
     return str1 + str2
