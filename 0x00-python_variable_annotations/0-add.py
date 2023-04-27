fea0e2bc658f88f9090e1f75e8ffb38ac8f20173
Bakare Muideen Adeleke <51156057+adeleke123@users.noreply.github.com>
2023-04-27 10:34:00 +0100

    python variable annotations

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-"""Module that contains a single function that adds two integers."""
+"""Module that contains a single function that adds two integers"""
 
 
 def add(a: float, b: float) -> float:
-    """ Add two integers together"""
+    """ Add two integers together."""
     return a + b
