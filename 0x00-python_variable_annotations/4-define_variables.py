ee9ca0ed7bc7a9dcbc4d909c66c8e832db3a276d
Bakare Muideen Adeleke <51156057+adeleke123@users.noreply.github.com>
2023-04-27 10:33:12 +0100

    python variables annotations

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Contains definition of variables with specified values and annotations."""
+"""Contains definition of variables with specified values and annotations"""
 
 a: int = 1
 pi: float = 3.14
